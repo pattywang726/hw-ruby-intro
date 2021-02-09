d46d3fa9e5d8de48f3e0043dd953477f192ff164
EC2 Default User <ec2-user@ip-172-31-8-136.us-east-2.compute.internal>
2021-02-09 20:19:03 +0000

    HW1 completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,86 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0) {|sum, number| sum += number}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.length == 1
+  arrL = arr.sort{ |a, b| b <=> a }.first(2)
+  return arrL.reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  
+  hashT = Hash.new
+  arr.each do |i| 
+    return true if (hashT.has_key?(n - i) )
+    hashT.store(i, "")
+    end
+  
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, ".concat(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if (s.empty?)
+  return false if (/[^a-zA-Z0-9_]/.match(s[0]) != nil)
+  return false if (/[aeiouAEIOU]/.match(s[0]) != nil)
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if (s.empty?)
+  return false if (/[^0-1]/.match(s) != nil)
+  
+  l = s.length
+  num = 0
+  s.each_char do |i|
+    num += (2**(l - 1)) * Integer(i) 
+    l -= 1
+  end
+  return true if (num % 4 == 0)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    
+    raise ArgumentError.new("Non valid ISBN number") if (isbn.empty?)
+    raise ArgumentError.new("Non valid book price") if (price <= 0)
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    @price = price
+  end
+  
+  def price_as_string
+    
+    return "$" + "%.2f" % price
+    
+  end
+  
 end
